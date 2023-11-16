cc0981d3ef7432f64de4ac1b17760aa6b23ecb93
kaidoverse <kaidoverse@gmail.com>
2023-11-16 16:02:01 +0000

    meta tags added

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <img src="/Logo.svg" alt="Little Lemon Logo" />
+            <h1>Little Lemon</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <main>
+            <p></p>
+        </main>
+    );
+}
+
+export default Main;
