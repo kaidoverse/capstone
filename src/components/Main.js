e6997e518b94a23ad47b67472f3e726054d7313f
kaidoverse <kaidoverse@gmail.com>
2023-12-05 12:04:22 +0000

    changes-to-submit-form

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import Header from './Header';
 import BookingPage from './BookingPage';
+import ConfirmedBooking from './ConfirmedBooking';
 
 const Main = () => {
 
@@ -52,7 +53,7 @@ const Main = () => {
                 <Route path='/' element={<Header />} />
                 <Route path='/booking' element={<BookingPage availableTimes={state} dispatch={dispatch} submitForm=
                     {submitForm} />} />
-                <Route path='/' element={<Header />} />
+                <Route path='/confirmed' element={<ConfirmedBooking />} />
             </Routes>
         </main>
     )
